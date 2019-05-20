7ba53d41f72001ba65ddf6c373409a531beea315
Stuart Muyambi <stuartmuyambi@gmail.com>
2019-05-20 10:57:55 +0300

    Remove trailing spaces

--- upload.php
+++ upload.php
@@ -2,11 +2,11 @@
 if (isset($_POST['submit'])) {
 	$file = $_FILES['file'];
 
-	$fileName = $_FILES['file'] ['name']; 
-	$fileTmpName = $_FILES['file'] ['tmp_name']; 
-	$fileSize = $_FILES['file'] ['size']; 
-	$fileError = $_FILES['file'] ['error']; 
-	$fileType = $_FILES['file'] ['type']; 
+	$fileName = $_FILES['file'] ['name'];
+	$fileTmpName = $_FILES['file'] ['tmp_name'];
+	$fileSize = $_FILES['file'] ['size'];
+	$fileError = $_FILES['file'] ['error'];
+	$fileType = $_FILES['file'] ['type'];
 
 	$fileExt = explode('.', $fileName);
 	$fileActualExt = strtolower(end($fileExt));
