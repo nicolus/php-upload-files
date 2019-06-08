0c9353da637b7104e2eb014f9978b5d9abbe8710
Stuart Muyambi <stuartmuyambi@gmail.com>
2019-06-08 17:07:40 +0300

    Add bootstrap

--- index.php
+++ index.php
@@ -2,12 +2,13 @@
 <html>
 <head>
 	<title>Upload Files with php</title>
+	<link href="bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
 	<h1>Upload Files to your php app</h1>
 	<form action="upload.php" method="POST" enctype="multipart/form-data">
 		<input type="file" name="file">
-		<button type="submit" name="submit">UPLOAD</button>
+		<button type="submit" class="btn-primary"name="submit">UPLOAD</button>
 	</form>
 </body>
 </html> 
